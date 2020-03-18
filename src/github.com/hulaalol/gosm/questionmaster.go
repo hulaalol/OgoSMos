af0676f241299761dd6baada73fc8841b1c150ae
Marcel Hasenbalg <marcel.hasenbalg@googlemail.com>
2020-03-18 15:39:34 +0100

    implemented conversion from json-answer to go object structure

--- src/github.com/hulaalol/gosm/questionmaster.go
+++ src/github.com/hulaalol/gosm/questionmaster.go
@@ -5,10 +5,64 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"strconv"
 	"strings"
 
 	"github.com/valyala/fastjson"
 )
+
+type information struct {
+	typ info
+	val info
+}
+
+type info struct {
+	typ string
+	val string
+}
+
+func getJSON(url string) *fastjson.Value {
+
+	resp, err := http.Get(url)
+	if err != nil {
+		// handle error
+		fmt.Println("error http request")
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	t := string(body)
+
+	var p fastjson.Parser
+	v, err := p.Parse(t)
+	return v
+}
+
+func queryDBP(item string) []information {
+
+	Url, err := url.Parse("https://dbpedia.org")
+	if err != nil {
+		panic("boom")
+	}
+
+	// Raw query because DBpedia query special characters should NOT be html encoded
+	Url.Path += "/sparql"
+	Url.RawQuery = "default-graph-uri=http://dbpedia.org&query=select+distinct+?property+?value%7B%0D%0A++dbr%3A" + item + "+%3Fproperty+%3Fvalue%0D%0A%7D&format=application%2Fsparql-results%2Bjson&CXML_redir_for_subjs=121&CXML_redir_for_hrefs=&timeout=30000&debug=on&run=+Run+Query+"
+	//fmt.Println("Querying DBPedia with: " + Url.String())
+
+	// create json from json-string answer
+	var data = getJSON(Url.String()).Get("results", "bindings").GetArray()
+
+	// convert json structure to go-objects array (information array)
+	var res []information
+	for _, d := range data {
+
+		var p = info{string(d.Get("property").GetStringBytes("type")), string(d.Get("property").GetStringBytes("value"))}
+		var v = info{string(d.Get("value").GetStringBytes("type")), string(d.Get("value").GetStringBytes("value"))}
+		res = append(res, information{p, v})
+	}
+
+	fmt.Println("Found " + strconv.Itoa(len(res)) + " informations about " + item)
+	return res
+}
 
 var stopwords = [...]string{"weg", "straße", "strasse", "allee", "gasse", "Straße", "Weg", "Strasse", "Allee"}
 var syllables = [...]string{"er"}
@@ -28,56 +82,4 @@ func cleanStreetname(s string) string {
 		}
 	}
 	return s
-}
-
-func queryDBP(item string) *fastjson.Value {
-
-	Url, err := url.Parse("https://dbpedia.org")
-	if err != nil {
-		panic("boom")
-	}
-
-	Url.Path += "/sparql"
-	Url.RawQuery = "default-graph-uri=http://dbpedia.org&query=select+distinct+?property+?value%7B%0D%0A++dbr%3A" + item + "+%3Fproperty+%3Fvalue%0D%0A%7D&format=application%2Fsparql-results%2Bjson&CXML_redir_for_subjs=121&CXML_redir_for_hrefs=&timeout=30000&debug=on&run=+Run+Query+"
-
-	fmt.Printf("Encoded URL is %q\n", Url.String())
-
-	fmt.Println(Url.String())
-	var result = getJSON(Url.String())
-
-	//fmt.Println(result)
-
-	return result
-
-}
-
-func getJSON(url string) *fastjson.Value {
-
-	resp, err := http.Get(url)
-
-	if err != nil {
-		// handle error
-		fmt.Println("error http request")
-	}
-	body, err := ioutil.ReadAll(resp.Body)
-
-	t := string(body)
-	fmt.Println(t)
-
-	var p fastjson.Parser
-	v, err := p.Parse(t)
-
-	return v
-}
-
-func testGet(url string) string {
-	resp, err := http.Get(url)
-
-	if err != nil {
-		// handle error
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-
-	return string(body)
 }
