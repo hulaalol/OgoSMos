c32cf948f620015fd3c043e35406582d3902c6fe
Hulaalol <marci.vfb4ever@googlemail.com>
2021-10-19 16:42:37 +0200

    update mapbox api and dbpedia blacklist

--- src/github.com/hulaalol/gosm/web/main.js
+++ src/github.com/hulaalol/gosm/web/main.js
@@ -252,12 +252,26 @@ $(document).ready(function(){
     });
 
 
+    //API changed 19.10.2021
+    /*
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 20,
         id: 'mapbox.streets',
         accessToken: 'pk.eyJ1IjoiaHVsYWFsb2wiLCJhIjoiY2szYjBqc2Q3MGhuazNkbXhrbnZsaHIyYiJ9._R-lsR0wjcmJGXf5TRmSSw'
     }).addTo(mymap);
+    */
+
+     //new API
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
+    tileSize: 512,
+    maxZoom: 20,
+    zoomOffset: -1,
+    id: 'mapbox/streets-v11',
+    accessToken: 'pk.eyJ1IjoiaHVsYWFsb2wiLCJhIjoiY2szYjBqc2Q3MGhuazNkbXhrbnZsaHIyYiJ9._R-lsR0wjcmJGXf5TRmSSw'
+}).addTo(mymap);
+
 
 
     initMap()
